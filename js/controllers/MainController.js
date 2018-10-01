8617e05042d859715d87553b1e18cf96847a20da
beigid <beigid@seattleu.edu>
2018-09-30 19:38:16 -0700

    First commit

---
+++ js/controllers/MainController.js
@@ -0,0 +1,29 @@
+app.controller('MainController', ['$scope', function($scope) {$scope.title='Yo'; $scope.promo="Bro"; 
+$scope.products = [{
+name: 'The Book of Trees', 
+price: 19, 
+pubdate: new Date('2014', '03', '08'), 
+cover: 'img/the-book-of-trees.jpg',
+likes: 0}, 
+{
+name: 'Program or be Programmed',
+price: 8,
+pubdate: new Date('2013', '08', '01'),
+cover: 'img/program-or-be-programmed.jpg',
+likes: 0},
+{
+name: 'Puppy Paradise',
+price: 7,
+pubdate: new Date('2016','08','10'),
+cover: 'img/program-or-be-programmed.jpg',
+likes: 0},
+{
+name: 'Puppy Paradise',
+price: 7,
+pubdate: new Date('2016','08','10'),
+cover: 'img/program-or-be-programmed.jpg',
+likes: 0}];
+$scope.plusOne = function(yo) {
+  $scope.products[yo].likes += 1;
+}
+}])                                                           
