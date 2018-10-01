8db9c5b4c9d32b709cc32fb8bedc3606e262cb71
beigid <beigid@seattleu.edu>
2018-09-30 23:49:28 -0700

    Adjusted dislike bar and new poster images

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,29 +1,50 @@
-app.controller('MainController', ['$scope', function($scope) {$scope.title='Yo'; $scope.promo="Bro"; 
+app.controller('MainController', ['$scope', function($scope) {$scope.title=''; $scope.promo="List of Highest rated movies based on IMDB scores"; 
 $scope.products = [{
-name: 'The Book of Trees', 
-price: 19, 
-pubdate: new Date('2014', '03', '08'), 
-cover: 'img/the-book-of-trees.jpg',
-likes: 0}, 
+name: 'The Shawshank Redemption', 
+price: 8.99, 
+pubdate: new Date('1994'), 
+cover: 'img/ShawshankRedemptionMoviePoster.jpg',
+likes: 0,
+dislikes:0}, 
 {
-name: 'Program or be Programmed',
-price: 8,
-pubdate: new Date('2013', '08', '01'),
-cover: 'img/program-or-be-programmed.jpg',
-likes: 0},
+name: 'The Godfather',
+price: 21,
+pubdate: new Date('1970', '02', '24'),
+cover: 'img/TheGodfatherMoviePoster.jpg',
+likes: 0,
+dislikes:0},
 {
-name: 'Puppy Paradise',
-price: 7,
+name: 'The Dark Knight',
+price: 5.99,
+pubdate: new Date('2007','07','18'),
+cover: 'img/TheDarkKnightMoviePoster.jpg',
+likes: 0,
+dislikes:0},
+{
+  name: 'The Godfather Part 2',
+  price: 9.99,
+  pubdate: new Date('1994','07','06'),
+  cover: 'img/TheGodfather2MoviePoster.jpg',
+  likes: 0,
+  dislikes:0},
+{
+  name: 'Pulp Fiction',
+  price: 12,
+  pubdate: new Date('1994','09','14'),
+  cover: 'img/PulpFictionMoviePoster.png',
+  likes: 0,
+  dislikes:0},
+{
+name: "Schindler's List",
+price: 7.79,
 pubdate: new Date('2016','08','10'),
-cover: 'img/program-or-be-programmed.jpg',
-likes: 0},
-{
-name: 'Puppy Paradise',
-price: 7,
-pubdate: new Date('2016','08','10'),
-cover: 'img/program-or-be-programmed.jpg',
-likes: 0}];
+cover: 'img/SchindlersListMoviePoster.jpg',
+likes: 0,
+dislikes:0}];
 $scope.plusOne = function(yo) {
   $scope.products[yo].likes += 1;
+}
+$scope.minusOne = function(index) {
+  $scope.products[index].dislikes += 1;
 }
 }])                                                           
